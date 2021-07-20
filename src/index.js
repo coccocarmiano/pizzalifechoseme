48deaabf1886d5f695a2bc1620bd544da9362312
coccocarmiano <s248992@studenti.polito.it>
2021-07-20 19:08:20 +0200

    fixed for real

--- src/index.js
+++ src/index.js
@@ -8,14 +8,7 @@ window.document.title = 'Pizza Life Chose Me?'
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <meta name="type" content="website" />
-      <meta name="title" content="Pizza Life" />
-      <meta name="description" content="Scopri se la Pizza Life ti ha scelto." />
-      <meta name="author" content="Cocco"/>
-      <meta name="image" content="https://blog.papajohns.co.uk/wp-content/uploads/2015/12/Pizza-Thumbnail.jpg" />
       <Main/>
-    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
